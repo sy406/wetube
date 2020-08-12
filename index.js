3455a8e360a152c7c47dba868da9cf580b27c89f
김서영 <kimseoyoung@MacBook-Pro.local>
2020-08-12 14:32:40 +0900

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hi!");
+import express from "express";
+import morgan from "morgan";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => 
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hello from home');
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.get("/", betweenHome, handleHome);
+
+app.use(betweenHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
