035dae60807309f1cca6279e6bbf69d93c0160fd
김서영 <kimseoyoung@MacBook-Pro.local>
2020-08-13 17:23:18 +0900

    Add partials

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
-app.set('view engine',"pug");
+app.use(helmet());
+app.set("view engine","pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+
+
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
