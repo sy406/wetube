174686e90a828a5d59fbbed642675cf0b22e41ea
김서영 <43196103+sy406@users.noreply.github.com>
2020-08-13 15:24:48 +0900

    Update videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,5 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
+export const search = (req, res) => res.render("search");
 export const videos = (req, res) => res.send("Videos");
 export const upload = (req, res) => res.send("Upload");
 export const videoDetail = (req, res) => res.send("Video Detail");
