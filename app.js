71b87e9a42b2239a201a75d07dcbb6a6ccaeb168
sy406 <kimseoyoung@MacBook-Pro.local>
2020-08-15 23:46:15 +0900

    Add header pug

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 const app = express();
 
 app.use(helmet());
@@ -15,8 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan("dev"));
-
-
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
